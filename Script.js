2c3738d759071a6227b08e50f445dfee419c5ead
JuanAngel08 <55809359+JuanAngel08@users.noreply.github.com>
2022-08-17 23:38:43 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+function Mostrar1(){ 
+
+document.getElementById("Parrafo1").style.display = "block";
+document.getElementById("Parrafo2").style.display = "none";
+
+document.getElementsById("ColElergirnosPrueba2").style.setProperty("background-color", "blue"); 
+ }
+
+ function Mostrar2(){ 
+
+    document.getElementById("Parrafo1").style.display = "none";
+    document.getElementById("Parrafo2").style.display = "block";
+     
+     }
